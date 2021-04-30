79467bec03acad8979b12de1230130ca775def6a
lilaligo <aligocz486@gmail.com>
2021-04-30 12:48:12 +0200

    Added new files

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+function gofaq() {
+    document.getElementById('faqel').click()
+}
+
+function goabout() {
+    document.getElementById('aboutel').click()
+}
+
+document.getElementById('home').style.height = window.innerHeight
+
+document.body.scrollTop = 0; // For Safari
+document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Thanks for purchasing my resource, 
+if you have any questions please contact me: eon#6562.
+Don't forget to leave a review - it means a lot!
